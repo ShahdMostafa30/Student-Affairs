ee3ce2686090d7990d842a7ba4cd2b459107192e
maryamosama33 <maryamosama3383@gmail.com>
2023-05-24 15:50:25 +0300

    Update search.js

--- project with django/studentaffairs/studentaffairs_app/static/studentaffairs_app/js/search.js
+++ project with django/studentaffairs/studentaffairs_app/static/studentaffairs_app/js/search.js
@@ -1,52 +1,66 @@
-//const students = JSON.parse(localStorage.getItem('students')) || [];
-const tableBody = document.getElementById('table-body');
-const searchInput = document.getElementById('search');
-const searchButton = document.getElementById('search-button');
-// display students
-let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    let students = JSON.parse(this.responseText);
+const studentList = document.getElementById("student-list");
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
 
 
-    for (let i = 0; i < students.length; i++) {
-      let student = students[i];
-      let row = document.createElement("tr");
-
-      
-      row.innerHTML = `
-            <td>${student.name}</td>
-            <td>${student.student_id}</td>
-            <td>${student.department}   <button onclick="editDepartment('${student.student_id}', '${student.name}', '${student.department}')">Edit</button></td>
-            <td>${student.email}</td>
-            <td>${student.gpa}</td>
-            <td>${student.phone}</td>
-            <td>${student.date_of_birth}</td>
-            <td>${student.level}</td>
-            <td>${student.status}</td>
-            <td><button onclick="editStudent('${student.student_id}', '${student.name}', '${student.department}','${student.level}', '${student.gpa}', '${student.email}', '${student.phone}', '${student.date_of_birth}', '${student.gender}', '${student.status}')">Edit</button></td>
-        `;
-      // Add the row to the table
-      tableBody.appendChild(row);
+function displayStudents() {
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let students = JSON.parse(this.responseText);
+      renderStudents(students);
     }
-  }
-};
-
-xhr.open("GET", "/get-students/", true);
-xhr.send();
-
-
-// edit department
-function editDepartment(studentId, studentName, studentDepartment) {
-  // redirect to the student's editing page, passing the ID, name, and department as URL parameters
-  const url = `/StuDepartment?id=${studentId}&name=${encodeURIComponent(studentName)}&Department=${encodeURIComponent(studentDepartment)}`;
-  window.location.href = url;
+  };
+  xhr.open("GET", "/get-students/", true);
+  xhr.send();
 }
-function editStudent(studentId, studentName, studentDepartment, studentLevel, studentGpa, studentEmail,
-  studentPhone, studentDateOfBirth, studentGender, studentStatus) {
-  const url = `/edit?id=${studentId}&name=${encodeURIComponent(studentName)}&department=${encodeURIComponent(studentDepartment)}&level=${encodeURIComponent(studentLevel)}&gpa=${encodeURIComponent(studentGpa)}&email=${encodeURIComponent(studentEmail)}&phone=${encodeURIComponent(studentPhone)}&dob=${encodeURIComponent(studentDateOfBirth)}&gender=${encodeURIComponent(studentGender)}&status=${encodeURIComponent(studentStatus)}`;
-  window.location.href = url;
+
+
+function renderStudents(students) {
+  studentList.innerHTML = ""; 
+  students.forEach((student) => {
+    let row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${student.name}</td>
+      <td>${student.student_id}</td>
+      <td>${student.level}</td>
+      <td>${student.department}</td>
+      <td>${student.gpa}</td>
+      <td>${student.email}</td>
+      <td>${student.phone}</td>
+      <td>${student.date_of_birth}</td>
+      <td>${student.gender}</td>
+      <td>${student.status}</td>
+      <td><button onclick="editStudent('${student.student_id}', '${student.name}', '${student.department}', '${student.level}', '${student.gpa}', '${student.email}', '${student.phone}', '${student.date_of_birth}', '${student.gender}', '${student.status}')">Edit</button></td>
+    `;
+    studentList.appendChild(row);
+  });
 }
+
+
+function searchStudents() {
+  const searchParam = searchInput.value.toLowerCase();
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let students = JSON.parse(this.responseText);
+      const filteredStudents = students.filter(
+        (student) =>
+          student.name.toLowerCase().includes(searchParam) ||
+          student.department.toLowerCase().includes(searchParam)
+      );
+      renderStudents(filteredStudents);
+    }
+  };
+  xhr.open("GET", `/search-students/?search_param=${searchParam}`, true);
+  xhr.send();
+}
+
+searchButton.addEventListener("click", searchStudents);
+
+
+displayStudents();
+
 
 
 // function searchStudents() {
